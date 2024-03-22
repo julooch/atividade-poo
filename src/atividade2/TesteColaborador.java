a98e5b582efb260d67e973795920892dc764f03f
Julia Domingues <142114663+julooch@users.noreply.github.com>
2024-03-21 21:36:05 -0300

    Resolução

--- src/atividade2/TesteColaborador.java
+++ src/atividade2/TesteColaborador.java
@@ -7,15 +7,12 @@ public class TesteColaborador {
         Colaborador colaborador1 = new Colaborador("Marco", "Gomes", 5000.0);
         Colaborador colaborador2 = new Colaborador("Julia", "Silva", 6000.0);
 
-        // Exibindo salário anual de cada colaborador
         System.out.println("Salário anual de " + colaborador1.getPrimeiroNome() + " " + colaborador1.getSobrenome() + ": " + colaborador1.calcularSalarioAnual());
         System.out.println("Salário anual de " + colaborador2.getPrimeiroNome() + " " + colaborador2.getSobrenome() + ": " + colaborador2.calcularSalarioAnual());
 
-        // Dando um aumento de 10% para cada colaborador
         colaborador1.aumentarSalario(10);
         colaborador2.aumentarSalario(10);
-
-        // Exibindo novamente o salário anual de cada colaborador após o aumento
+        
         System.out.println("\nApós aumento de 10%:");
         System.out.println("Salário anual de " + colaborador1.getPrimeiroNome() + " " + colaborador1.getSobrenome() + ": " + colaborador1.calcularSalarioAnual());
         System.out.println("Salário anual de " + colaborador2.getPrimeiroNome() + " " + colaborador2.getSobrenome() + ": " + colaborador2.calcularSalarioAnual());
