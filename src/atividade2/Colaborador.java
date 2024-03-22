d5c43d45b89ab4ef73f329fa95cb8527aa38b45f
Julia Domingues <142114663+julooch@users.noreply.github.com>
2024-03-21 21:17:13 -0300

    Resolução de exercícios

--- src/atividade2/Colaborador.java
+++ src/atividade2/Colaborador.java
@@ -1,37 +1,47 @@
 package atividade2;
 
 public class Colaborador {
-    private String nome;
-    private String sobrenome;
-    private double salario;
+        private String primeiroNome;
+        private String sobrenome;
+        private double salarioMensal;
 
-    public Colaborador(String nome, String sobrenome, double salario) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.salario = (salario >= 0) ? salario : 0.0;
+        public Colaborador(String primeiroNome, String sobrenome, double salarioMensal) {
+            this.primeiroNome = primeiroNome;
+            this.sobrenome = sobrenome;
+            this.salarioMensal = (salarioMensal >= 0.0) ? salarioMensal : 0.0;
+        }
+
+        public String getPrimeiroNome() {
+            return primeiroNome;
+        }
+
+        public void setPrimeiroNome(String primeiroNome) {
+            this.primeiroNome = primeiroNome;
+        }
+
+        public String getSobrenome() {
+            return sobrenome;
+        }
+
+        public void setSobrenome(String sobrenome) {
+            this.sobrenome = sobrenome;
+        }
+
+        public double getSalarioMensal() {
+            return salarioMensal;
+        }
+
+        public void setSalarioMensal(double salarioMensal) {
+            this.salarioMensal = (salarioMensal >= 0.0) ? salarioMensal : 0.0;
+        }
+
+        public double calcularSalarioAnual() {
+            return salarioMensal * 12;
+        }
+
+        public void aumentarSalario(double percentual) {
+            salarioMensal *= (1 + percentual / 100);
+        }
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
 }
